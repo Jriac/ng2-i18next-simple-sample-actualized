fd74c7dc4f24a95c6bdaa836260e6e33de2d1413
Jriac <jriac91@gmail.com>
2019-09-10 14:23:45 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import * as i18nextBrowserLanguageDetector from 'i18next-browser-languagedetecto
 import {I18nDirective} from 'ng2-i18next/ng2-i18next';
 
 
-const NG2_I18NEXT_PROVIDERS = [{ provide: I18nServiceConfig, useValue: { use: [i18nextBrowserLanguageDetector, i18nextXHRBackend], config: { detection: { order: ['navigator'] }, fallbackLng: 'en' } } }, I18nService]
+const NG2_I18NEXT_PROVIDERS = [{ provide: I18nServiceConfig, useValue: { use: [i18nextBrowserLanguageDetector, i18nextXHRBackend], config: { detection: { order: ['navigator'] }, fallbackLng: 'es' } } }, I18nService]
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
